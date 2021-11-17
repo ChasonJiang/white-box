18bd5e75a3fab075feb19de82ff14b404abcf9e7
ChasonJiang <1440499136@qq.com>
2021-11-17 21:57:08 +0800

    new file:   .browserslistrc 	new file:   .editorconfig 	new file:   .eslintrc.json 	new file:   .gitignore 	new file:   angular.json 	new file:   capacitor.config.ts 	new file:   e2e/protractor.conf.js 	new file:   e2e/src/app.e2e-spec.ts 	new file:   e2e/src/app.po.ts 	new file:   e2e/tsconfig.json 	new file:   ionic.config.json 	new file:   karma.conf.js 	new file:   package-lock.json 	new file:   package.json 	new file:   src/app/app-routing.module.ts 	new file:   src/app/app.component.html 	new file:   src/app/app.component.scss 	new file:   src/app/app.component.spec.ts 	new file:   src/app/app.component.ts 	new file:   src/app/app.module.ts 	new file:   src/app/home/home-routing.module.ts 	new file:   src/app/home/home.module.ts 	new file:   src/app/home/home.page.html 	new file:   src/app/home/home.page.scss 	new file:   src/app/home/home.page.spec.ts 	new file:   src/app/home/home.page.ts 	new file:   src/assets/icon/favicon.png 	new file:   src/assets/shapes.svg 	new file:   src/environments/environment.prod.ts 	new file:   src/environments/environment.ts 	new file:   src/global.scss 	new file:   src/index.html 	new file:   src/main.ts 	new file:   src/polyfills.ts 	new file:   src/test.ts 	new file:   src/theme/variables.scss 	new file:   src/zone-flags.ts 	new file:   tsconfig.app.json 	new file:   tsconfig.json 	new file:   tsconfig.spec.json

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'games-community-and-store',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
