3164561f3255a3b6f185aae0c306b70d0098d261
shirly0154 <164921960@qq.com>
2021-12-19 20:12:32 +0800

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/interface 	modified:   src/app/me-components/usermessage/usermessage.component.html 	modified:   src/app/me-components/usermessage/usermessage.component.ts 	modified:   src/app/me/usersetting/usersetting.page.ts

--- src/app/me-components/usermessage/usermessage.component.ts
+++ src/app/me-components/usermessage/usermessage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserInfo } from 'src/app/interface/User';
+import { getCurrentUserCard } from 'src/app/util/util';
 @Component({
   selector: 'app-usermessage',
   templateUrl: './usermessage.component.html',
@@ -7,11 +8,11 @@ import { UserInfo } from 'src/app/interface/User';
 })
 export class UsermessageComponent implements OnInit {
 
-  user:UserInfo =     {
-    uid:"164921960",
-    userName:'柴浩',
+  user:UserInfo = {
+    uid:getCurrentUserCard().uid,
+    userName:getCurrentUserCard().userName,
     signature:'好果汁，你让我疯狂',
-    numberOfFollow: 1561,
+    numberOfFollow: getCurrentUserCard().userLevel,
     numberOfFans: 191,
     numberOfCollection: 5699,
     userLevel:6,
